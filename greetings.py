dd6fa9830aff3d81ac35badeff16ef200c1bddce
PhilippeRemoue <philippe.remoue77@gmail.com>
2026-01-13 09:53:35 +0100

    Add greeting message and user input functionality

---
+++ greetings.py
@@ -0,0 +1,3 @@
+print("Je suis en train d'apprendre à utiliser Git et Github et j'adore ça !")
+nom = input("Quel est ton nom ? ")
+print(f"Enchanté, {nom} ! Passe une bonne journée en codant avec Git et Github.")
